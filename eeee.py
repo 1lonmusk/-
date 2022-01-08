b852c46cfe75ac17ec236f6c64953b3567247c08
1lonmusk <96938029+1lonmusk@users.noreply.github.com>
2022-01-08 17:51:24 +0900

    Add files via upload

---
+++ eeee.py
@@ -0,0 +1,60 @@
+# print ("강한친구 대한육군\n강한친구 대한육군")
+#
+# a = int(input())
+# b = int(input())
+# print (a*(b%10))
+# print (a*(b//10%10))
+# print (a*(b//10//10))
+# print (a*b)
+#
+# r = " qwe "
+# print (r.strip())
+# print (r.upper())
+#
+# i = "life is too short"
+# print (i.replace("life", "Your leg"))
+# print (i.split())
+# j = "life:is:too:short"
+# print (j.split(:))
+#
+# z = "btc_krw"
+# print (z.upper())
+# print (z.split("_"))
+#
+# x = "039490    "
+# print (x.rsplit())
+#
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     print ("a는 b보다 크다")
+#
+# a = 6
+# b = 4
+# if a%2==0 and b%2==0:
+#     print("a와b는 짝수다.")
+#
+# a = int(input())
+# b = int(input())
+# if (a + b > 8):
+#     pass
+#
+# if a>b:
+#     print ("a는 b보다 크다")
+# else:
+#     print ("a는 b보다 작거나 같다")
+#
+# a = int(input())
+# b = int(input())
+# if a>b:
+#     print (">")
+# if a<b:
+#     print ("<")
+# if a==b:
+#     print ("==")
+#
+# s = int(input())
+# if s>89:
+# if s<89>0:
+# print ("A")
+# print ("B")
